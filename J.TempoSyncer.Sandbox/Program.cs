1640c854b182c4797d4d277e2ce059ddbae32f09
jelle <jelle@DESKTOP-JELLE>
2025-09-07 13:09:26 +0200

    Add project files.

---
+++ J.TempoSyncer.Sandbox/Program.cs
@@ -0,0 +1,9 @@
+﻿using J.TempoSyncer.Core.Utility.Config;
+
+ApplicationConfig applicationConfig = new()
+{
+    FilePathFFMPEG = "C:/Data/Utilities/Video/FFMPEG-v8.0/bin/ffmpeg.exe",
+    FilePathFFProbe = "C:/Data/Utilities/Video/FFMPEG-v8.0/bin/ffprobe.exe",
+};
+
+ApplicationConfig.WriteConfig(applicationConfig, "config.json");
